0e5396fdb7a192f6958fedda0a4cea9c1c0bd92c
Pietro Rampazzo <pietro@rampazzo.co>
2018-11-23 15:05:13 +0100

    importing library

--- app/src/main/java/it/unive/dais/commandroid/MainActivity.java
+++ app/src/main/java/it/unive/dais/commandroid/MainActivity.java
@@ -4,10 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import it.unive.dais.legodroid.lib.EV3;
+//import it.unive.dais.legodroid.lib.comm.BluetoothConnection;
+//import it.unive.dais.legodroid.lib.plugs.GyroSensor;
+//import it.unive.dais.legodroid.lib.plugs.LightSensor;
+//import it.unive.dais.legodroid.lib.plugs.Plug;
+//import it.unive.dais.legodroid.lib.plugs.TachoMotor;
+//import it.unive.dais.legodroid.lib.plugs.TouchSensor;
+//import it.unive.dais.legodroid.lib.plugs.UltrasonicSensor;
+//import it.unive.dais.legodroid.lib.util.Consumer;
+//import it.unive.dais.legodroid.lib.util.Prelude;
+//import it.unive.dais.legodroid.lib.util.ThrowingConsumer;
 
 public class MainActivity extends AppCompatActivity {
-
-    EV3 ev3 = new EV3(new BluetoothConnection("EV3").connect());
+    
+    private EV3 ev3 = new EV3(new BluetoothConnection("EV3").connect());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
