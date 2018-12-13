fe11fccb620ac8e3a6ecc0effd05476127e2deb6
Pietro Rampazzo <pietro@rampazzo.co>
2018-12-13 13:36:36 +0100

    Update MainActivity.java

--- app/src/main/java/it/unive/dais/commandroid/MainActivity.java
+++ app/src/main/java/it/unive/dais/commandroid/MainActivity.java
@@ -3,7 +3,10 @@ package it.unive.dais.commandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
 import it.unive.dais.legodroid.lib.EV3;
+import it.unive.dais.legodroid.lib.plugs.TachoMotor;
 //import it.unive.dais.legodroid.lib.comm.BluetoothConnection;
 //import it.unive.dais.legodroid.lib.plugs.GyroSensor;
 //import it.unive.dais.legodroid.lib.plugs.LightSensor;
@@ -16,6 +19,11 @@ import it.unive.dais.legodroid.lib.EV3;
 //import it.unive.dais.legodroid.lib.util.ThrowingConsumer;
 
 public class MainActivity extends AppCompatActivity {
+
+    EV3 ev3 = new EV3(new BluetoothConnection("EV3").connect());
+
+    public MainActivity() throws IOException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
