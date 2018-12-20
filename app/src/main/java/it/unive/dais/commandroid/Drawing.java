58b76bb46988d313ffaec78deb82d516d2c87252
Pietro Rampazzo <pietro@rampazzo.co>
2018-12-20 13:59:01 +0100

    removing table in drawing activity

--- app/src/main/java/it/unive/dais/commandroid/Drawing.java
+++ app/src/main/java/it/unive/dais/commandroid/Drawing.java
@@ -1,5 +1,6 @@
 package it.unive.dais.commandroid;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -25,5 +26,4 @@ public class Drawing extends AppCompatActivity {
             }
         });
     }
-
 }
