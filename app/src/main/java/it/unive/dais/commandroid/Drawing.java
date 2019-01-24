c1cdda65270beffe2c71f9ae09097b3b68544fc1
GiorgioBoscoloB <45979402+GiorgioBoscoloB@users.noreply.github.com>
2019-01-24 18:55:39 +0100

    Funzione draw funzionante

--- app/src/main/java/it/unive/dais/commandroid/Drawing.java
+++ app/src/main/java/it/unive/dais/commandroid/Drawing.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import java.io.IOException;
 
@@ -20,9 +21,9 @@ import static it.unive.dais.legodroid.lib.plugs.TachoMotor.Type.MEDIUM;
 
 public class Drawing extends AppCompatActivity implements View.OnClickListener{
 
-    private static int RIGHE = 7;
-    private static int COLONNE = 4;
-    private Button[][] buttons = new Button[RIGHE][COLONNE];
+    private static int RIGHE = 8;
+    private static int COLONNE = 8;
+    private CheckBox[][] buttons = new CheckBox[RIGHE][COLONNE];
     protected int[][] draw = new int[RIGHE][COLONNE];
     TachoMotor motor;
     TachoMotor motor1;
@@ -44,7 +45,7 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         try {
@@ -57,7 +58,7 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
                     for(int y = 0; y < COLONNE; y++){
                         String buttonID = "b" + i + y;
                         int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-                        buttons[i][y] = (Button) findViewById(resID);
+                        buttons[i][y] =  findViewById(resID);
                         buttons[i][y].setOnClickListener(this);
                         draw[i][y] = 0;
                     }
@@ -82,18 +83,18 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for(int i = 0; i < RIGHE; i++){
+        for(int i =RIGHE-1; i >=0; i--){
             for(int y = 0; y < COLONNE; y++){
                 if(draw[i][y]==1){
                     try {
-                        motor.setStepPower(20,0,360,0,true);
+                        motor.setStepPower(80,0,360,0,true);
                         motor.waitCompletion();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
                 try {
-                    motor1.setStepPower(-20,0,45,0,true);
+                    motor1.setStepPower(-70,0,30,0,true);
                     motor1.waitCompletion();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -101,23 +102,27 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
 
             }
             try {
-                motor1.setStepPower(20,0,180,0,true);
+                motor1.setStepPower(70,0,240,0,true);
                 motor1.waitCompletion();
-                motor2.setStepPower(20,0,90,0,true);
+                motor2.setStepPower(70,0,15,0,true);
                 motor2.waitCompletion();
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
         }
+        try {
+            motor2.setStepPower(20,0,1000,0,true);
+            motor2.waitCompletion();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public void ButtonsOnClick(Button button, int x, int y){
         if(draw[x][y] == 0){
-            buttons[x][y].setText("X");
             draw[x][y] = 1;
         }
         else{
-            buttons[x][y].setText("");
             draw[x][y] = 0;
         }
     }
@@ -138,6 +143,19 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
             case R.id.b03:
                 ButtonsOnClick(buttons[0][3], 0, 3);
                 break;
+            case R.id.b04:
+                ButtonsOnClick(buttons[0][4], 0, 4);
+                break;
+            case R.id.b05:
+                ButtonsOnClick(buttons[0][5], 0, 5);
+                break;
+            case R.id.b06:
+                ButtonsOnClick(buttons[0][6], 0, 6);
+                break;
+            case R.id.b07:
+                ButtonsOnClick(buttons[0][7], 0, 7);
+                break;
+
             //riga 1
             case R.id.b10:
                 ButtonsOnClick(buttons[1][0], 1, 0);
@@ -151,6 +169,20 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
             case R.id.b13:
                 ButtonsOnClick(buttons[1][3], 1, 3);
                 break;
+            case R.id.b14:
+                ButtonsOnClick(buttons[1][4], 1, 4);
+                break;
+            case R.id.b15:
+                ButtonsOnClick(buttons[1][5], 1, 5);
+                break;
+            case R.id.b16:
+                ButtonsOnClick(buttons[1][6], 1, 6);
+                break;
+            case R.id.b17:
+                ButtonsOnClick(buttons[1][7], 1, 7);
+                break;
+
+
             //riga 2
             case R.id.b20:
                 ButtonsOnClick(buttons[2][0], 2, 0);
@@ -163,6 +195,18 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
                 break;
             case R.id.b23:
                 ButtonsOnClick(buttons[2][3], 2, 3);
+                break;
+            case R.id.b24:
+                ButtonsOnClick(buttons[2][4], 2, 4);
+                break;
+            case R.id.b25:
+                ButtonsOnClick(buttons[2][5], 2, 5);
+                break;
+            case R.id.b26:
+                ButtonsOnClick(buttons[2][6], 2, 6);
+                break;
+            case R.id.b27:
+                ButtonsOnClick(buttons[2][7], 2, 7);
                 break;
             //riga 3
             case R.id.b30:
@@ -177,6 +221,18 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
             case R.id.b33:
                 ButtonsOnClick(buttons[3][3], 3, 3);
                 break;
+            case R.id.b34:
+                ButtonsOnClick(buttons[3][4], 3, 4);
+                break;
+            case R.id.b35:
+                ButtonsOnClick(buttons[3][5], 3, 5);
+                break;
+            case R.id.b36:
+                ButtonsOnClick(buttons[3][6], 3, 6);
+                break;
+            case R.id.b37:
+                ButtonsOnClick(buttons[3][7], 3, 7);
+                break;
             //riga 4
             case R.id.b40:
                 ButtonsOnClick(buttons[4][0], 4, 0);
@@ -189,6 +245,18 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
                 break;
             case R.id.b43:
                 ButtonsOnClick(buttons[4][3], 4, 3);
+                break;
+            case R.id.b44:
+                ButtonsOnClick(buttons[4][4], 4, 4);
+                break;
+            case R.id.b45:
+                ButtonsOnClick(buttons[4][5], 4, 5);
+                break;
+            case R.id.b46:
+                ButtonsOnClick(buttons[4][6], 4, 6);
+                break;
+            case R.id.b47:
+                ButtonsOnClick(buttons[4][7], 4, 7);
                 break;
             //riga 5
             case R.id.b50:
@@ -203,6 +271,18 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
             case R.id.b53:
                 ButtonsOnClick(buttons[5][3], 5, 3);
                 break;
+            case R.id.b54:
+                ButtonsOnClick(buttons[5][4], 5, 4);
+                break;
+            case R.id.b55:
+                ButtonsOnClick(buttons[5][5], 5, 5);
+                break;
+            case R.id.b56:
+                ButtonsOnClick(buttons[5][6], 5, 6);
+                break;
+            case R.id.b57:
+                ButtonsOnClick(buttons[5][7], 5, 7);
+                break;
             //riga 6
             case R.id.b60:
                 ButtonsOnClick(buttons[6][0], 6, 0);
@@ -216,6 +296,49 @@ public class Drawing extends AppCompatActivity implements View.OnClickListener{
             case R.id.b63:
                 ButtonsOnClick(buttons[6][3], 6, 3);
                 break;
+            case R.id.b64:
+                ButtonsOnClick(buttons[6][4], 6, 4);
+                break;
+                case R.id.b65:
+                ButtonsOnClick(buttons[6][5], 6, 5);
+                break;
+            case R.id.b66:
+                ButtonsOnClick(buttons[6][6], 6, 6);
+                break;
+            case R.id.b67:
+                ButtonsOnClick(buttons[6][7], 6, 7);
+                break;
+            //riga 7
+            case R.id.b70:
+                ButtonsOnClick(buttons[7][0], 7, 0);
+                break;
+                case R.id.b71:
+                ButtonsOnClick(buttons[7][1], 7, 1);
+                    break;
+
+            case R.id.b72:
+                ButtonsOnClick(buttons[7][2], 7, 2);
+                break;
+
+            case R.id.b73:
+                ButtonsOnClick(buttons[7][3], 7, 3);
+                break;
+
+            case R.id.b74:
+                ButtonsOnClick(buttons[7][4], 7, 4);
+                break;
+
+            case R.id.b75:
+                ButtonsOnClick(buttons[7][5], 7,5 );
+                break;
+
+            case R.id.b76:
+                ButtonsOnClick(buttons[7][6], 7, 6);
+                break;
+            case R.id.b77:
+                ButtonsOnClick(buttons[7][7], 7, 7);
+                break;
+
             default:
                 break;
         }
